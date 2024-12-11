4071a1df4c54e0a8e81c590e1345470d98f947c2
mehdinathani <mehdinathani@gmail.com>
2024-12-11 20:29:25 +0500

    first commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function WebHeader() {
+    return (
+        <header className="header">
+            <h1 className="bg-black text-3xl text-white bg-center text-center items-center h-20 justify-center flex font-extrabold">Todo List</h1>
+        </header>
+
+    )
+
+}
